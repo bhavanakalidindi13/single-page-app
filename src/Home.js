1b067b737020bd5cc2fa4f6d6ab4172e83417411
bhavanakalidindi13 <bhavanakalidindi1e@gmail.com>
2021-06-23 03:13:58 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>
+         A single-page application (SPA) is a web application
+         or web site that interacts with the user by dynamically
+         rewriting the current page rather than loading entire
+         new pages from a server.
+        </p>
+
+        <p> That was a simple definition of SPA- Single Page Application. </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/Stuff.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>STUFF</h2>
+        <p> Few Social Media platforms: </p>
+        <ol>
+          <li> Twitter </li>
+          <li> LinkedIn</li>
+          <li> Facebook </li>
+          <li> Instagram </li>
+          <li> SnapChat </li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Stuff;
